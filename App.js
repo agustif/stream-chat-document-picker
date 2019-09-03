04e1a8ab05a5ed0d8f5ffaf8f0468251e09a00cf
Agusti Fernandez <me@agusti.me>
2019-09-03 22:15:41 +0200

    Updating some deps

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 
 
-import React from "react";
-import Reactotron from 'reactotron-react-native'
-import { View, SafeAreaView } from "react-native";
-import { StreamChat } from "stream-chat";
-import {
-  Chat,
-  Channel,
-  MessageList,
-  MessageInput,
-} from "stream-chat-expo";
+// import React from "react";
+// import { View, SafeAreaView } from "react-native";
+// import { StreamChat } from "stream-chat";
+// import {
+//   Chat,
+//   Channel,
+//   MessageList,
+//   MessageInput,
+// } from "stream-chat-expo";
 
 // require('dotenv').config();
+import Reactotron from 'reactotron-react-native'
 import { STREAM_CHAT_CLIENT, STREAM_CHAT_KEY } from 'react-native-dotenv'
 
 Reactotron
@@ -19,28 +19,83 @@ Reactotron
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
 
-const chatClient = new StreamChat(STREAM_CHAT_CLIENT);
-const userToken = STREAM_CHAT_KEY;
+// const chatClient = new StreamChat(STREAM_CHAT_CLIENT);
+// const userToken = STREAM_CHAT_KEY;
+import React, { PureComponent } from 'react';
+import { View, SafeAreaView, Text } from 'react-native';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  MessageList,
+  MessageInput,
+  ChannelPreviewMessenger,
+  ChannelList,
+} from 'stream-chat-expo';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const chatClient = new StreamChat('f8wwud5et5jd');
+const userToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiYXV0dW1uLWJyZWV6ZS0yIn0.KGEUf4XzbAvFcnnOzPaMFN2WBxkLNPB8Gb4lKiv9jZs';
 
 const user = {
-  id: 'broad-fire-5',
-  name: 'Broad fire',
+  id: 'autumn-breeze-2',
+  name: 'Autumn breeze',
   image:
     'https://stepupandlive.files.wordpress.com/2014/09/3d-animated-frog-image.jpg',
 };
 
 chatClient.setUser(user, userToken);
 
-class ChannelScreen extends React.Component {
+class ChannelListScreen extends PureComponent {
+  static navigationOptions = () => ({
+    headerTitle: (
+      <Text style={{ fontWeight: 'bold' }}>Awesome Conversations</Text>
+    ),
+  });
+
   render() {
-    const channel = chatClient.channel("messaging", "broad-fire-5");
-    channel.watch();
+    return (
+      <SafeAreaView>
+        <Chat client={chatClient}>
+          <View style={{ display: 'flex', height: '100%', padding: 10 }}>
+            <ChannelList
+              filters={{ type: 'messaging', members: { $in: ['autumn-breeze-2'] } }}
+              sort={{ last_message_at: -1 }}
+              Preview={ChannelPreviewMessenger}
+              onSelect={(channel) => {
+                this.props.navigation.navigate('Channel', {
+                  channel,
+                });
+              }}
+            />
+          </View>
+        </Chat>
+      </SafeAreaView>
+    );
+  }
+}
+
+class ChannelScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    const channel = navigation.getParam('channel');
+    return {
+      headerTitle: (
+        <Text style={{ fontWeight: 'bold' }}>{channel.data.name}</Text>
+      ),
+    };
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const channel = navigation.getParam('channel');
 
     return (
       <SafeAreaView>
         <Chat client={chatClient}>
-          <Channel channel={channel}>
-            <View style={{ display: "flex", height: "100%" }}>
+          <Channel client={chatClient} channel={channel}>
+            <View style={{ display: 'flex', height: '100%' }}>
               <MessageList />
               <MessageInput />
             </View>
@@ -51,8 +106,24 @@ class ChannelScreen extends React.Component {
   }
 }
 
+const RootStack = createStackNavigator(
+  {
+    ChannelList: {
+      screen: ChannelListScreen,
+    },
+    Channel: {
+      screen: ChannelScreen,
+    },
+  },
+  {
+    initialRouteName: 'ChannelList',
+  },
+);
+
+const AppContainer = createAppContainer(RootStack);
+
 export default class App extends React.Component {
   render() {
-    return <ChannelScreen />;
+    return <AppContainer />;
   }
 }
